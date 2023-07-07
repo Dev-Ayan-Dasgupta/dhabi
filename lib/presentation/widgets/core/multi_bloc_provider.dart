f3fed74fd458df77f9437300876ce70e735122c4
Ayan-Qolaris <128606356+Ayan-Qolaris@users.noreply.github.com>
2023-07-07 17:00:49 +0530

    build 51 for SIT

--- lib/presentation/widgets/core/multi_bloc_provider.dart
+++ lib/presentation/widgets/core/multi_bloc_provider.dart
@@ -63,7 +63,7 @@ class _CustomMultiBlocProviderState extends State<CustomMultiBlocProvider> {
       // ? environment string
       String environment = const String.fromEnvironment(
         'ENVIRONMENT',
-        defaultValue: Environment.dev,
+        defaultValue: Environment.stag,
       );
 
       // ? load environment file
